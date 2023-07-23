78931ef6adf89e54f423626f41ffb99f141b43b7
Sidney Filho <sidinhocostafilho@gmail.com>
2023-07-23 15:11:10 +0100

    commit de ajustes na aplicação

--- js/favorites.js
+++ js/favorites.js
@@ -3,26 +3,83 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root);
+    this.load()
   }
+
+  load() {
+    this.entries = [
+      {
+        login: "maykbrito",
+        name: "Mayk Brito",
+        public_repos: "76",
+        followers: "12000",
+      },
+      {
+        login: "Sidney-Filho",
+        name: "Sidney",
+        public_repos: "76",
+        followers: "12000",
+      },
+    ];
+   }
 }
+
 //classe que vai criar a visualização e eventos do html
 export class FavoritesView extends Favorites {
   constructor(root) {
-    super(root)
+    super(root);
 
-    this.update()
+    this.tbody = this.root.querySelector("table tbody");
+
+    this.update();
   }
 
-  update () {
-    this.removeAllTr()
+  update() {
+    this.removeAllTr();
+
+    this.entries.forEach((user) => {
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      this.tbody.append(row)
+    });
+
+  }
+
+  createRow() {
+    const tr = document.createElement("tr");
+
+    tr.innerHTML = `
+          <td class="user">
+            <img src="https://github.com/Sidney-Filho.png" alt="">
+            <a href="https://github.com/Sidney-Filho" target="_blank">
+              <p>Sidney-Filho</p>
+              <span>Sidney-Filho</span>
+            </a>
+          </td>
+          <td class="repositories">
+            76
+          </td>
+          <td class="followers">
+            42
+          </td>
+          <td>
+            <button class="remove">&times;</button>
+          </td>
+    `;
+
+    return tr;
   }
 
   removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
-
-    tbody.querySelectorAll('tr')
-      .forEach((tr) => {
-        tr.remove()
-      })
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
   }
 }
